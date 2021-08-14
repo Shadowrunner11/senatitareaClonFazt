ee0bdc00ce0c4ba6084cdd391ada098d1adf2d4b
Shadow <pieroramirez810@gmail.com>
2021-08-13 17:17:30 -0700

    Aniadimos formato

--- controller.py
+++ controller.py
@@ -5,23 +5,29 @@ from tkinter import TclError, Tk
 
 class Controller:
     def __init__(self):
-        self.App=App(Tk())
+        self.App = App(Tk())
         self.refresh_table()
-        self.App.btnGuardar["command"]=self.insertar
-        self.App.btnDel["command"]=self.borrar
-        self.App.btnActu["command"]=self.update
+        self.App.btnGuardar["command"] = self.insertar
+        self.App.btnDel["command"] = self.borrar
+        self.App.btnActu["command"] = self.update
         self.App.mainloop()
+
     def refresh_table(self):
-        a=self.App.tabla
+        a = self.App.tabla
         for nombre, precio, marca, cantidad, id in getProducts():
-            a.item(a.insert(parent="", index="end", text=nombre), values=[precio, marca, cantidad, id])
+            a.item(
+                a.insert(parent="", index="end", text=nombre),
+                values=[precio, marca, cantidad, id],
+            )
+
     def insertar(self):
-        a={}
+        a = {}
         try:
-            a=self.App.info
+            a = self.App.info
         except TclError:
-            self.App.lblAviso["text"]="Datos no validos"
-        flag=True
+            self.App.lblAviso["text"] = "Datos no validos"
+        flag = True
+
         def helper():
             try:
                 insertProduct(**a)
@@ -30,22 +36,26 @@ class Controller:
                 return "Insertando"
             except:
                 return "Datos no validos"
-            
+
         for value in a.values():
-            flag=flag and len(value)
-        self.App.lblAviso["text"]=helper() if flag else "Campos vacios"
-        
+            flag = flag and len(value)
+        self.App.lblAviso["text"] = helper() if flag else "Campos vacios"
+
     def borrar(self):
-        a=self.App.tabla
+        a = self.App.tabla
         for item in a.selection():
             delProduct(int((a.item(item, "values")[-1])))
         self.App.tabla.delete(*self.App.tabla.get_children())
         self.refresh_table()
+
     def update(self):
-        a=self.App.tabla
-        actuProduct(int(a.item(a.selection()[0], "values")[-1]), self.App.info["precio"])
+        a = self.App.tabla
+        actuProduct(
+            int(a.item(a.selection()[0], "values")[-1]), self.App.info["precio"]
+        )
         self.App.tabla.delete(*self.App.tabla.get_children())
         self.refresh_table()
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     Controller()
--- view.py
+++ view.py
@@ -1,56 +1,60 @@
-from tkinter import Frame, Label, Button, Entry, Tk, Variable, ttk, StringVar, LabelFrame, DoubleVar, IntVar
+from tkinter import (
+    Frame,
+    Label,
+    Button,
+    Entry,
+    Tk,
+    Variable,
+    ttk,
+    StringVar,
+    LabelFrame,
+    DoubleVar,
+    IntVar,
+)
 
 
 class App(Frame):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self._info=self.datos(parent)
-        self.lblAviso=Label(parent, text="")
+        self._info = self.datos(parent)
+        self.lblAviso = Label(parent, text="")
         self.lblAviso.pack()
-        cuadro=Frame(parent)
-        self.tabla=ttk.Treeview(cuadro, columns=["1", "2", "3", "4"], displaycolumns=["1", "2", "3"])
+        cuadro = Frame(parent)
+        self.tabla = ttk.Treeview(
+            cuadro, columns=["1", "2", "3", "4"], displaycolumns=["1", "2", "3"]
+        )
         self.tabla.grid(row=0, columnspan=2)
-        
+
         cuadro.pack()
-        self.btnDel=Button(cuadro, text="Borrar")
+        self.btnDel = Button(cuadro, text="Borrar")
         self.btnDel.grid(column=0, row=1, sticky="we")
-        self.btnActu=Button(cuadro, text="Update")
+        self.btnActu = Button(cuadro, text="Update")
         self.btnActu.grid(column=1, row=1, sticky="we")
-    def datos(self, parent)->dict:
-        lbfInfo=LabelFrame(parent, text="Informacion")
+
+    def datos(self, parent) -> dict:
+        lbfInfo = LabelFrame(parent, text="Informacion")
         lbfInfo.pack()
-        info={
-            "nombre": (producto:=StringVar()),
-            "precio": (precio:=DoubleVar()),
-            "marca": (marca:=StringVar()),
-            "cantidad": (cantidad:=IntVar())
-        }        
+        info = {
+            "nombre": (producto := StringVar()),
+            "precio": (precio := DoubleVar()),
+            "marca": (marca := StringVar()),
+            "cantidad": (cantidad := IntVar()),
+        }
         self.componente(lbfInfo, "Producto", producto)
         self.componente(lbfInfo, "Precio", precio, 1)
-        self.componente(lbfInfo, "Marca", marca,2)
+        self.componente(lbfInfo, "Marca", marca, 2)
         self.componente(lbfInfo, "Cantidad", cantidad, 3)
-        self.btnGuardar=Button(lbfInfo, text="Guardar", command=lambda : print("Hola guillermo"))
+        self.btnGuardar = Button(
+            lbfInfo, text="Guardar", command=lambda: print("Hola guillermo")
+        )
         self.btnGuardar.grid(row=4, columnspan=2, sticky="we")
 
         return info
-    def componente(self,parent, text:str, var:Variable, row=0):
+
+    def componente(self, parent, text: str, var: Variable, row=0):
         Label(parent, text=text).grid(column=0, row=row)
         Entry(parent, textvariable=var).grid(column=1, row=row)
 
     @property
-    def info(self)->dict:
+    def info(self) -> dict:
         return {key: value.get() for key, value in self._info.items()}
-
-
-
-
-
-
-        
-
-    
-
-    
-
-        
-
